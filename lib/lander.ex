45e22f189bd6418058a3a62237ecf8ec22afdf79
cysc0 <cpgast@gmail.com>
2019-04-04 14:11:27 -0400

    :(

--- lib/lander.ex
+++ lib/lander.ex
@@ -1,6 +1,6 @@
 defmodule Lander do
   @moduledoc """
-  HuskyShop keeps the contexts that define your domain
+  Lander keeps the contexts that define your domain
   and business logic.
 
   Contexts are also responsible for managing your data, regardless
